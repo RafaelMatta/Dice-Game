cd380cd0d1ef61f9df0a0ad470f957fb4a2d7c45
RaffoMatto <rafaelbibianomatta@gmail.com>
2022-02-22 13:16:58 -0300

    Informações de player e adição de animações.

--- js/player.js
+++ js/player.js
@@ -1,6 +1,10 @@
 const activePlayer = document.querySelector('.player--active');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
+const current0El = document.querySelector('.current--0');
+const current1El = document.querySelector('.current--1');
+
+const currentActive = document.querySelector('current');
 
 let currentPlayer = 0;
 
@@ -10,11 +14,16 @@ addEventListener('keydown', function (e) {
     }
 
     if (currentPlayer === 0) {
-        player0El.classList.add("player--active--0");
-        player1El.classList.remove("player--active--1");
+        player0El.classList.add('player--active--0');
+        player1El.classList.remove('player--active--1');
+        current0El.classList.add('current--active');
+        current1El.classList.remove('current--active');
+
     } else if (currentPlayer === 1) {
-        player1El.classList.add("player--active--1");
-        player0El.classList.remove("player--active--0");
+        player1El.classList.add('player--active--1');
+        player0El.classList.remove('player--active--0');
+        current1El.classList.add('current--active');
+        current0El.classList.remove('current--active');
     }
 
     console.log(currentPlayer);
