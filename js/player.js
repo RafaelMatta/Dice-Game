bcfc0a7056c9fe68f2c50833288d59286bfce910
RaffoMatto <rafaelbibianomatta@gmail.com>
2022-02-25 01:28:41 -0300

    Adicionando e estilizando botões hold e roll.

--- js/player.js
+++ js/player.js
@@ -3,6 +3,8 @@ const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const current0El = document.querySelector('.current--0');
 const current1El = document.querySelector('.current--1');
+const control0El = document.querySelector('.control--0');
+const control1El = document.querySelector('.control--1');
 
 const currentActive = document.querySelector('current');
 
@@ -13,18 +15,12 @@ addEventListener('keydown', function (e) {
         currentPlayer = currentPlayer === 0 ? 1 : 0;
     }
 
-    if (currentPlayer === 0) {
-        player0El.classList.add('player--active--0');
-        player1El.classList.remove('player--active--1');
-        current0El.classList.add('current--active');
-        current1El.classList.remove('current--active');
-
-    } else if (currentPlayer === 1) {
-        player1El.classList.add('player--active--1');
-        player0El.classList.remove('player--active--0');
-        current1El.classList.add('current--active');
-        current0El.classList.remove('current--active');
-    }
+    player0El.classList.toggle('player--active--0');
+    player1El.classList.toggle('player--active--1');
+    current0El.classList.toggle('hidden');
+    current1El.classList.toggle('hidden');
+    control0El.classList.toggle('hidden');
+    control1El.classList.toggle('hidden');
 
     console.log(currentPlayer);
 });
